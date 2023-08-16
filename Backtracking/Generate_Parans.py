ca63f6ec5495682e443075b1110bd4c934981921
ayush2725 <67007088+ayush2725@users.noreply.github.com>
2023-08-16 11:09:13 -0700

    Update Generate_Parans.py

--- Backtracking/Generate_Parans.py
+++ Backtracking/Generate_Parans.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        def back(curr, o, c):
+            if len(curr) == n*2:
+                ans.append(curr)
+                return
+            if o < n:
+                back(curr + "(", o + 1, c)
+            if c < o:
+                back(curr + ')', o, c + 1)
+        back('', 0, 0)
+        return ans
+        
